d3b2a3db2fe7d88f365fbd80c474318f1d5525a2
tiagocosta2103 <tctiacos@gmail.com>
2026-02-26 12:19:52 +0000

    replace edit icon with lucide pencil icon

--- src/app.js
+++ src/app.js
@@ -7014,9 +7014,9 @@ function renderBlocklists() {
             </div>
           </div>
           <button class="blocklist-action-btn edit-btn" title="Edit">
-            <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-              <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-              <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+            <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/>
+              <path d="m15 5 4 4"/>
             </svg>
           </button>
         </div>
