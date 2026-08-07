a64701c074e83d381abbe5a8f00024949ca88d16
tiagocosta2103 <tctiacos@gmail.com>
2026-08-07 14:21:36 +0100

    update windows publisher display name

--- scripts/build-msix.ps1
+++ scripts/build-msix.ps1
@@ -40,7 +40,7 @@ if (-not $IdentityName -or -not $Publisher) {
 }
 
 if (-not $PublisherDisplayName) {
-    $PublisherDisplayName = "Reduce Digital Distraction"
+    $PublisherDisplayName = "Centre for Digital Habits"
 }
 
 Write-Host "  App: Digital Habits: Blocker v$AppVersion" -ForegroundColor White
